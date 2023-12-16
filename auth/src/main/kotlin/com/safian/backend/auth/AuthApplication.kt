6957d0724f0c4c549e21e05c6d4724104e77f9ed
Szabolcs Sáfián <ssafian@loginet.hu>
2023-12-16 20:00:01 +0100

    jwt auth

--- auth/src/main/kotlin/com/safian/backend/auth/AuthApplication.kt
+++ auth/src/main/kotlin/com/safian/backend/auth/AuthApplication.kt
@@ -1,13 +1,72 @@
 package com.safian.backend.auth
 
+import com.safian.backend.auth.model.Role
+import com.safian.backend.auth.model.RoleName
+import com.safian.backend.auth.model.User
+import com.safian.backend.auth.services.RoleService
+import com.safian.backend.auth.services.UserService
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.Bean
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @SpringBootApplication
 @EnableDiscoveryClient
-class AuthApplication
+@ConfigurationPropertiesScan
+class AuthApplication {
+
+	@Bean
+	fun initDatabase(
+		roleService: RoleService,
+		userService: UserService,
+	) = CommandLineRunner {
+		setupInitialDatabase(
+			roleService = roleService,
+			userService = userService,
+			passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder(),
+		)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<AuthApplication>(*args)
 }
+
+private fun setupInitialDatabase(
+	roleService: RoleService,
+	userService: UserService,
+	passwordEncoder: PasswordEncoder,
+) {
+	roleService.createRoleIfNotExists(role = Role(roleName = RoleName.USER))
+	roleService.createRoleIfNotExists(role = Role(roleName = RoleName.ADMIN))
+
+	val userRole = roleService.findRoleByName(RoleName.USER)
+	val adminRole = roleService.findRoleByName(RoleName.ADMIN)
+
+	if (userRole != null) {
+		userService.createUserIfNotExists(
+			User(
+				name = "User",
+				email = "user@test.com",
+				pwd = passwordEncoder.encode("user-password"),
+				roles = listOf(userRole),
+			),
+		)
+	}
+
+	if (userRole != null && adminRole != null) {
+		userService.createUserIfNotExists(
+			User(
+				name = "Admin",
+				email = "admin@test.com",
+				pwd = passwordEncoder.encode("admin-password"),
+				roles = listOf(userRole, adminRole),
+			),
+		)
+	}
+}
+
